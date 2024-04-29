5cb915f986da27cc0696206bdcfc0430142ed0a4
Daniel Castro <dcastro@ualberta.ca>
2024-04-28 22:08:39 -0600

    Enable package installations via MELPA

--- init.el
+++ init.el
@@ -4,3 +4,8 @@
   (load-file user-init-file))
 
 (global-set-key (kbd "C-c C-l") 'reload-init-file)    ; Reload .emacs file
+
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
+(add-to-list 'package-archives
+	                  '("melpa-stable" . "https://stable.melpa.org/packages/") t)
