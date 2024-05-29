b42481de9c361c8cebe78d7ea832b30d5a541ccd
Daniel Castro <dcastro@ualberta.ca>
2024-05-28 23:30:27 -0600

    Added and configure lsp and ivy packages

--- init.el
+++ init.el
@@ -12,14 +12,34 @@
 
 (require 'package)
 (add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
-(add-to-list 'package-archives
-	                  '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("melpa-stable" . "https://stable.melpa.org/packages/") t)
+(add-to-list 'package-archives '("org" . "https://orgmode.org/elpa/") t)
+(add-to-list 'package-archives '("gnu" . "https://elpa.gnu.org/packages/") t)
+(add-to-list 'package-archives '("nongnu" . "https://elpa.nongnu.org/nongnu/") t)
+
+(package-initialize)
+(unless package-archive-contents
+  (package-refresh-contents))
+
+  ;; Initialize use-package on non-Linux platforms
+(unless (package-installed-p 'use-package)
+  (package-install 'use-package))
+
+(require 'use-package)
+(setq use-package-always-ensure t)
+
+(setq inhibit-startup-message t)
+
+(column-number-mode)
+(global-display-line-numbers-mode t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
- '(package-selected-packages '(lsp-mode)))
+ '(package-selected-packages
+   '(lsp-ivy ivy-rich ivy which-key lsp-treemacs lsp-ui gnu-elpa-keyring-update ## lsp-mode)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
@@ -27,8 +47,51 @@
  ;; If there is more than one, they won't work right.
  )
 
-;; lsp-mode settings.
-(setq lsp-keymap-prefix "s-l")
+(use-package which-key
+  :init (which-key-mode)
+  :diminish which-key-mode
+  :config
+  (setq which-key-idle-delay 1))
 
-(require 'lsp-mode)
-(add-hook 'prog-mode-hook #'lsp)
+(use-package ivy
+  :diminish
+  :bind (("C-s" . swiper)
+         :map ivy-minibuffer-map
+         ("TAB" . ivy-alt-done)
+         ("C-l" . ivy-alt-done)
+         ("C-j" . ivy-next-line)
+         ("C-k" . ivy-previous-line)
+         :map ivy-switch-buffer-map
+         ("C-k" . ivy-previous-line)
+         ("C-l" . ivy-done)
+         ("C-d" . ivy-switch-buffer-kill)
+         :map ivy-reverse-i-search-map
+         ("C-k" . ivy-previous-line)
+         ("C-d" . ivy-reverse-i-search-kill))
+  :config
+  (ivy-mode 1))
+
+(use-package ivy-rich
+  :init
+  (ivy-rich-mode 1))
+
+;; lsp-mode settings.
+(setq lsp-keymap-prefix "C-c e")  ;; Or 'C-l', 's-l'
+(use-package lsp-mode
+  :commands (lsp lsp-deferred)
+  :bind-keymap ("C-c e" . lsp-command-map)
+  :hook (lsp-mode . my/lsp-mode-setup)
+  :init
+  :config
+  (lsp-enable-which-key-integration t))
+
+(use-package lsp-ui
+  :hook (lsp-mode . lsp-ui-mode)
+  :custom
+  (lsp-ui-peek-enable t)
+  (lsp-ui-doc-position 'at-point))
+
+(use-package lsp-treemacs
+  :after lsp)
+
+(use-package lsp-ivy)
